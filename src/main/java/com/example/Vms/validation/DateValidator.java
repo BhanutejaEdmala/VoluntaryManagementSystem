83df1a8d153a103dc76e215250323f9b9a3b3381
BhanutejaEdmala <bhanuteja.edmala@cjsstechnologies.com>
2024-03-19 17:41:17 +0530

    changes added

--- src/main/java/com/example/Vms/validation/DateValidator.java
+++ src/main/java/com/example/Vms/validation/DateValidator.java
@@ -9,12 +9,11 @@ import java.text.SimpleDateFormat;
 public class DateValidator implements ConstraintValidator<Date,String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        sdf.setLenient(false); // Set lenient to false to enforce strict date parsing
-
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+        date.setLenient(false); // Set lenient to false to enforce strict date parsing
         try {
             // Attempt to parse the date string
-            sdf.parse(value);
+            date.parse(value);
             return true; // If parsing is successful, the date is valid
         } catch (Exception e) {
             return false; // If an exception is thrown, the date is invalid
